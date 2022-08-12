427c37717e74247f49bc104ac4a061d1ddb1e0b5
Nithin Vasalamari <nithin.vasalamari@gmail.com>
2022-08-12 22:31:00 +0530

    Intial template and folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherComponentComponent } from './weather/weather-component/weather-component.component';
+import { NewNotesComponent } from './Notes/new-notes/new-notes.component';
+import { SavedNotesComponent } from './Notes/saved-notes/saved-notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponentComponent,
+    NewNotesComponent,
+    SavedNotesComponent
   ],
   imports: [
     BrowserModule
